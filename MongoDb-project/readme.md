bc4d1173be68e54960b925709c9a82807ecc151a
shivadxt <shivamweb3.0@gmail.com>
2023-12-29 12:50:18 +0530

    Mongo questions solved

--- MongoDb-project/readme.md
+++ MongoDb-project/readme.md
@@ -1,11 +1,11 @@
 Complete MongoDB :
 
-- How can I perform a case-insensitive search in Mongoose?
+- How to perform a case-insensitive search in Mongoose?
 
-- How do I find documents where an array field contains all of a set of values?
+- How to find documents where an array field contains all of a set of values?
 
-- How can I search for documents with a specific date range in Mongoose?
+- How to search for documents with a specific date range in Mongoose?
 
-- How can I filter documents based on the existence of a field in Mongoose?
+- How to filter documents based on the existence of a field in Mongoose?
 
-- How can I filter documents based on a specific field's length in Mongoose? I
+- How to filter documents based on a specific field's length in Mongoose? 
--- MongoDb-project/routes/index.js
+++ MongoDb-project/routes/index.js
@@ -9,12 +9,54 @@ router.get('/', function(req, res, next) {
 
 router.get('/create', async function(req,res){
   const user = await userModel.create({
-    userName:"Shivam Tiwari",
-    nickName:"Tiwendra",
-    description:"I love Food",
-    categories: ['Momos','Liti chokha','Pepsi']
+    userName:"Shivam",
+    nickName:"Shiv",
+    description:"I love travel",
+    categories: ['UK','HP']
   })
   res.send(user);
 });
+
+//How to perform a case-insensitive search in Mongoose?
+
+router.get('/find', async function(req,res){
+
+  try{
+  const regex = new RegExp("^shiVam$",'i');
+  const user  = await userModel.find({
+    userName:regex
+  });
+
+  if(user.length === 0){
+    //(users.length === 0) is a condition that checks whether the users array returned 
+    //by the userModel.find() method is empty.
+    res.send("User Not Found !");
+  }
+  else{
+    res.send(user);
+  }
+}
+  catch (error) {
+    // Handle any potential errors (e.g., database connection issues)
+    res.status(500).send("Error fetching user: " + error.message);
+  }
+});
+
+
+//How to find documents where an array field contains all of a set of values?
+router.get('/findUser',async function(req,res){
+  const prop = await userModel.find({categories: {$all:'TVS'}});
+  res.send(prop);
+})
+
+//How to search for documents with a specific date range in Mongoose?
+router.get('/findDate', async function(req,res){
+  const date1 = new Date('2023-12-29');
+  const date2 = new Date('2023-12-30');
+  const dateData = await userModel.find({dateCreated: {$gte:date1,$lte:date2}});
+  res.send(dateData);
+});
+
+//How can I filter documents based on the existence of a field in Mongoose?
 
 module.exports = router;
